a253183be3afc2c33569b8eb537620758c4e3866
huangdijia <huangdijia@gmail.com>
2019-10-22 15:23:30 +0800

    update

--- src/Http/Controllers/WebSoarController.php
+++ src/Http/Controllers/WebSoarController.php
@@ -3,14 +3,17 @@
 namespace Huangdijia\WebSoar\Http\Controllers;
 
 use Guanguans\SoarPHP\Soar;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Mail\Markdown;
 
 class WebSoarController
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('web-soar::web-soar', ['path' => config('web-soar.path')]);
+        return view('web-soar::web-soar', [
+            'path' => config('web-soar.path'),
+        ]);
     }
 
     public function execute(Request $request)
@@ -25,11 +28,15 @@ class WebSoarController
             ->all();
 
         $soar = new Soar($config);
+        $body = '';
 
-        if ($config['-report-type'] == 'markdown') {
-            return '<div class="markdown-body">' . Markdown::parse($soar->score($validated['code'])) . '</div>';
+        if (Str::is('*/explain', $request->server('HTTP_REFERER'))) {
+            $body = $soar->explain($validated['code'], 'md');
+        } else {
+            $body = $soar->score($validated['code']);
         }
+        
+        return '<div class="markdown-body">' . Markdown::parse($body) . '</div>';
 
-        return $soar->score($validated['code']);
     }
 }
